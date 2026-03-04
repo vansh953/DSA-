9b7243fde304bcea247ed961f556c9b127e55a67
vansh953 <vansh24164035@akgec.ac.in>
2026-03-04 17:51:11 +0530

    day 4

---
+++ num.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void arm(int n){
+  int o=n;
+int v=n;
+    int c=0;
+while(n>0){
+    n/=10;
+    c++;
+}
+int sum =0;
+while(o>0) {
+    int rem = o%10;
+    sum=sum+pow(rem,c);
+    o/=10;
+}
+if(sum==v){
+    cout<<" armstrong";
+}
+else{
+    cout<<"no";
+}
+}
+void div( int n){
+int i =0;
+for(i=1;i<sqrt(n);i++){
+if(n%i==0){
+    cout<<i<<" ";
+    if(n/i!=i){
+cout<<n/i<<" ";
+}
+}
+}
+}
+int main(){
+div(36);
+    return 0;
+}
